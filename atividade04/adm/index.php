dae7733f67fc0e7070d626d36a8a599619119377
Arthur José Fernandes <49981282+arthurjf@users.noreply.github.com>
2022-05-30 12:54:53 -0300

    Teste com bootstrap na tela de login

--- atividade04/adm/index.php
+++ atividade04/adm/index.php
@@ -4,25 +4,41 @@
 <html>
 
 <head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Login</title>
-    <meta charset="utf-8" />
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='../css/bootstrap.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='../css/app.css'>
+    <script src='js/bootstrap.bundle.js'></script>
 </head>
 
 <body>
-    <form method="post" action="login.php">
-        <p>
-            <label>Nome:</label>
-            <input type="text" name="txtNome" required />
-        </p>
-        <p>
-            <label>Senha:</label>
-            <input type="password" name="txtSenha" required />
-        </p>
-        <p>
-            <input type="submit" value="Entrar">
-            <input type="reset" value="Limpar">
-        </p>
-    </form>
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-header">Login</div>
+                    <div class="card-body">
+                        <form class="form-container form-group" method="post" action="login.php">
+                            <p>
+                                <label>Nome:</label>
+                                <input type="text" name="txtNome" required />
+                            </p>
+                            <p>
+                                <label>Senha:</label>
+                                <input type="password" name="txtSenha" required />
+                            </p>
+                            <div class="text-center">
+                                <p>
+                                    <input type="submit" value="Entrar" class="btn btn-primary">
+                                    <input type="reset" value="Limpar" class="btn btn-secondary">
+                                </p>
+                            </div>
+                        </form>
+                    </div>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
