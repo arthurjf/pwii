8b1901f53f491b1d0b9265ceadfda4e6bc7c0be3
Arthur José Fernandes <49981282+arthurjf@users.noreply.github.com>
2022-07-03 19:12:48 -0300

    Correcoes na tela de registrar

--- atividade05/adm/autenticacao/secoes/registrar.php
+++ atividade05/adm/autenticacao/secoes/registrar.php
@@ -1,20 +1,17 @@
 <div class="container h-100 registrar">
     <div class="row justify-content-md-center h-100">
         <div class="col-lg-3 col-md-6 col-sm-12 painel">
-            <h2 class="titulo">Registrar</h2>
-            <p class="subtitulo">Preencha os campos abaixo para criar uma conta de administrador</p>
+            <h2 class="titulo">Criar conta</h2>
+            <p class="subtitulo">Registrar conta de administrador</p>
             <form method="post" action="registrar.php">
                 </p>
-                <p>E-mail</p>
-                <p><input type="email" name="txtEmail" placeholder="Exemplo@123" required /></p>
-                <p>Nome</p>
-                <p><input type="text" name="txtNome" placeholder="Exemplo" required /></p>
-                <p>Senha</p>
-                <p><input type="password" name="txtSenha" required /></p>
+                <p><input type="email" name="txtEmail" placeholder="E-mail" required /></p>
+                <p><input type="text" name="txtNome" placeholder="Nome" required /></p>
+                <p><input type="password" name="txtSenha" placeholder="Senha" required /></p>
                 <p><input class="btn-submit" type="submit" value="Registrar" /></p>
             </form>
             <p><a href="../index.php"><button class="btn-cancelar">Cancelar</button></a></p>
-            <p class="criarconta">Já tem uma conta? <a href="?secao=login">Então logue aqui</a></p>
+            <p class="possui-conta">Já tem uma conta? <a href="?secao=login">Então logue aqui</a></p>
         </div>
     </div>
 </div>
